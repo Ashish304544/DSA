6b22c38e2f144dc88bb4ddff8707a4ad2ad42b04
Ashish Singh <singhashish856308@gmail.com>
2026-08-09 19:25:58 +0530

    Add palindrome checking method in Solution class
    Implement a method to check if a string is a palindrome, ignoring non-alphanumeric characters and case.

---
+++ Leet code solution/0019_valid_palindrome.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            // Non-alphanumeric characters skip karo
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            // Lowercase karke compare karo
+            if (Character.toLowerCase(s.charAt(left)) !=
+                Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
